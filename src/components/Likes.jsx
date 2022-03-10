84ef02110eefed21ff2729943a885b2446568c27
matt <mjones4114@outlook.com>
2022-03-10 14:22:53 +0000

    refactor like component

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -7,66 +7,50 @@ import {
   AiOutlineDislike,
   AiOutlineLike,
 } from "react-icons/ai";
-import './Likes.css'
+import "./Likes.css";
 
 function Likes({ setLikeErr, votes, setVotes, id, type }) {
-  const [like, setLike] = useState(null);
-  const [dislike, setDislike] = useState(null);
+  const [like, setLike] = useState(0);
 
   const handleLike = (value) => {
-    const incrementFunction = (type === "article") ? incrementArticle : incrementComment
-    let setFunction = setLike;
-    let comparisonValue = dislike;
-    let changeValue = 1;
-    let currentCheck = like;
-    if (value === "dislike") {
-      setFunction = setDislike;
-      comparisonValue = like;
-      changeValue = -1;
-      currentCheck = dislike;
+    const incrementFunction =
+      type === "article" ? incrementArticle : incrementComment;
+    if (like === 0 && (value === 1 || value === -1) ) {
+      setLike(() => value)
+      setVotes((votes) => votes + value)
+      incrementFunction({id:id, increment: value})
+      .catch(err => {
+        setVotes((votes) => votes - value)
+        setLikeErr("Failed to update likes");
+        setLike((like) => like - value)
+      })
+    } else if (like === value) {
+      setLike(() => 0)
+      setVotes((votes) => votes - value)
+      incrementFunction({id:id, increment: -value })
+      .catch(err => {
+        setVotes((votes) => votes + value)
+        setLikeErr("Failed to update likes");
+        setLike((like) => like + value)
+      })
     }
-    if (comparisonValue === false || comparisonValue === null) {
-      if (currentCheck === true) {
-        setFunction(false);
-        setVotes((votes) => votes - changeValue);
-        incrementFunction({
-          id: id,
-          increment: -changeValue,
-        }).catch((err) => {
-          setVotes((votes) => votes + changeValue);
-          setLikeErr("Failed to update likes");
-          setFunction(true);
-        });
-      } else {
-        setFunction(true);
-        setVotes((votes) => votes + changeValue);
-        incrementFunction({ id: id, increment: changeValue })
-          .catch((err) => {
-            setVotes((votes) => votes - changeValue);
-            setLikeErr("Failed to update likes");
-            setFunction(false);
-          });
-      }
-    }
+    
   };
-  
-  const likeIcon = like ? <AiFillLike /> : <AiOutlineLike />;
-  const dislikeIcon = dislike ? <AiFillDislike /> : <AiOutlineDislike />;
 
   return (
     <span className="likes__container">
       <button
-        onClick={() => handleLike("like")}
+        onClick={() => handleLike(1)}
         className="likes__container-button"
       >
-        {likeIcon}
+        {like === 1 ? <AiFillLike /> : <AiOutlineLike />}
       </button>
       <span className="likes__vote-count">{votes}</span>
       <button
-        onClick={() => handleLike("dislike")}
+        onClick={() => handleLike(-1)}
         className="likes__container-dislike-button"
       >
-        {dislikeIcon}
+        {like === -1 ? <AiFillDislike /> : <AiOutlineDislike />}
       </button>
     </span>
   );
